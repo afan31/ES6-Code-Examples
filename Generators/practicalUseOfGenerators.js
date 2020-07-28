34edc0b07fa17523d9af099632cd5437cc8ce788
afan31 <afan.khan50@gmail.com>
2020-07-28 19:29:42 -0400

    practical use of generators

---
+++ Generators/practicalUseOfGenerators.js
@@ -0,0 +1,23 @@
+const engineeringTeam = {
+  size: 3,
+  department: 'Engineering',
+  lead: 'Jill',
+  manager: 'Alex',
+  engineer: 'Dave'
+}
+
+
+// need is to iterate over all employees in engineering team (no need to know size or department)
+
+function* TeamIterator(team) {
+  yield team.lead;
+  yield team.manager;
+  yield team.engineer;
+}
+
+const names = [];
+for (let name of TeamIterator(engineeringTeam)) {
+  names.push(name);
+};
+
+console.log(names);
